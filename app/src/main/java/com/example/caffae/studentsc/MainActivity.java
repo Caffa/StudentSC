baea3cb5f79f82a57bb878fe075898ad07a7e7ba
Caffa <wangyeelin@gmail.com>
2018-02-03 04:02:40 +0800

    Working Blank Activity - Student Main Activity
    Working login screen (without database…)

---
+++ app/src/main/java/com/example/caffae/studentsc/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.caffae.studentsc;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.widget.EditText;
+
+import com.shashank.sony.fancytoastlib.FancyToast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * Created by Caffae on 3/2/18.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.loginScreenID)
+    EditText loginScreenID;
+    @BindView(R.id.loginScreenPW)
+    EditText loginScreenPW;
+    @BindView(R.id.loginCardView)
+    CardView loginCardView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.loginCardView)
+    public void onViewClicked() {
+
+        //DONE: Check against set values
+        String id = loginScreenID.getText().toString();
+        String password = loginScreenPW.getText().toString();
+
+        //TODO:Check against the Database
+
+        //TODO: this is a substitute so remove later
+        if (id.equals("1002222")){
+            if(password.equals("pwd")){
+                FancyToast.makeText(this,"Successful Login!",FancyToast.LENGTH_LONG,FancyToast.SUCCESS,true);
+
+                //TODO: go to next activity
+                Intent intent = new Intent(this, StudentMainActivity.class);
+                startActivity(intent);
+
+            }
+        }
+
+
+        //alternative
+        //DONE: wrong 'pwd/id' toast
+        FancyToast.makeText(this,"Wrong Id/Password !",FancyToast.LENGTH_LONG,FancyToast.ERROR,true);
+
+    }
+}
