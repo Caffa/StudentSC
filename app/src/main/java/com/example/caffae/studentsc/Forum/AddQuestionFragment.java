5d30f5ed91dc371f45dd603c3e467fb717535ad3
Caffa <wangyeelin@gmail.com>
2018-04-05 23:33:53 +0800

    Adding Q fixed :D
    multiple q adding works now

--- app/src/main/java/com/example/caffae/studentsc/Forum/AddQuestionFragment.java
+++ app/src/main/java/com/example/caffae/studentsc/Forum/AddQuestionFragment.java
@@ -5,7 +5,6 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,15 +15,6 @@ import android.widget.Toast;
 import com.example.caffae.studentsc.R;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
 
 public class AddQuestionFragment extends Fragment {
@@ -62,54 +52,55 @@ public class AddQuestionFragment extends Fragment {
                 }else{
 
                     mDatabase = FirebaseDatabase.getInstance().getReference();
-//                    DatabaseReference ref_carid = mDatabase.child("0").child("question");
-//                    final int[] counter = new int[1];
-//
-//
-//                    ref_carid.addListenerForSingleValueEvent(new ValueEventListener() {
-//                        @Override
-//                        public void onDataChange(DataSnapshot dataSnapshot) {
-//                            String sc = (String) dataSnapshot.getValue(String.class);
-//                            counter[0] = Integer.parseInt(sc);
-//                            Log.e("SC", "Error: " + sc);
-//                            Toast.makeText(getContext(), sc, Toast.LENGTH_SHORT).show();
-//                        }
-//
-//                        @Override
-//                        public void onCancelled(DatabaseError databaseError) {
-//
-//                        }
-//                    });
-//
-//                    int counter = 30;
-//                    int counter = Integer.parseInt(readFromFile(getContext()));
-//                    int mergeCounterStudentId = Integer.parseInt(counter)
 
-                    //for unique id of forum Q
-                    String countTxt = readFromFile(getContext());
+                    //get student ID
                     SharedPreferences sharedPrefStudent = getActivity().getSharedPreferences(getString(R.string.studentId), Context.MODE_PRIVATE);
                     int studentId = sharedPrefStudent.getInt(getString(R.string.studentId), 1000030);
-                    String myStudenId = String.valueOf(studentId - 1000000);
-                    int counter = Integer.parseInt(myStudenId + countTxt);
 
-//                    Toast.makeText(getContext(), lastQuery.toString(), Toast.LENGTH_SHORT).show();
-//                    String uniqueNode = mDatabase.push().getKey();
+
+                    //get counter ID
+                    SharedPreferences myCounterPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+//                    int countVar = myCounterPref.getInt(getString(R.string.counterQ), 1);
+//                    String countTxt = myCounterPref.getString(getString(R.string.counterQuestion).trim(), "1");
+                    Integer countvar = myCounterPref.getInt(getString(R.string.counterQuestion), 1);
+                    String countTxt = String.valueOf(countvar);
+
+                    //Increment and save
+                    int newCount = Integer.parseInt(countTxt) + 1;
+
+                    SharedPreferences.Editor editorC = myCounterPref.edit();
+                    editorC.putInt(getString(R.string.counterQuestion), newCount);
+                    editorC.commit();
+
+                    //merge the IDs
+                    String myStudentId = String.valueOf(studentId - 1000000);
+//                    int counter = Integer.parseInt(myStudentId.trim() + countTxt.trim());
+                    String mergedIds = myStudentId.trim() + countTxt.trim();
+
+                    //get classroom ID
+                    SharedPreferences sharedPref = getActivity().getSharedPreferences(getString(R.string.classroomID), Context.MODE_PRIVATE);
+                    String classroom = sharedPref.getString(getString(R.string.classroomID), "Classroom1");
+
+                    //Post
+
+
 
                     //Add the numberID for the student into this so can make it a unique ID
 
 
-                    SharedPreferences sharedPref = getActivity().getSharedPreferences(getString(R.string.classroomID), Context.MODE_PRIVATE);
-                    String classroom = sharedPref.getString(getString(R.string.classroomID), "Classroom1");
 
 
-                    int newCount = 1 + Integer.parseInt(countTxt);
-                    String myCountTxt = String.valueOf(newCount);
+
+//                    int newCount = 1 + Integer.parseInt(countTxt);
+//                    String myCountTxt = String.valueOf(newCount);
 //                    System.out.println(destination.getAbsolutePath());
 
-                    stringToFile(myCountTxt,"counter.txt");
+
+//                    stringToFile(myCountTxt,"counter.txt");
+
 
 //                    mDatabase.child(Integer.toString(counter)).child("question").setValue(questionTxt);
-                    mDatabase.child(classroom).child("Forum").child(Integer.toString(counter)).child("question").setValue(questionTxt);
+                    mDatabase.child(classroom).child("Forum").child(mergedIds).child("question").setValue(questionTxt);
 
                     Toast.makeText(getContext(), "Question Submitted", Toast.LENGTH_SHORT).show();
                     FragmentManager manager;
@@ -129,68 +120,68 @@ public class AddQuestionFragment extends Fragment {
 
 
     }
-
-    private String readFromFile(Context context) {
-
-        String ret = "";
-
-        try {
-            InputStream inputStream = context.openFileInput("counter.txt");
-
-            if ( inputStream != null ) {
-                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-                String receiveString = "";
-                StringBuilder stringBuilder = new StringBuilder();
-
-                while ( (receiveString = bufferedReader.readLine()) != null ) {
-                    stringBuilder.append(receiveString);
-                }
-
-                inputStream.close();
-                ret = stringBuilder.toString();
-            }
-        }
-        catch (FileNotFoundException e) {
-            Log.e("postQ activity", "File not found: " + e.toString());
-        } catch (IOException e) {
-            Log.e("postQ activity", "Can not read file: " + e.toString());
-        }catch (NullPointerException e){
-            e.printStackTrace();
-        }
-
-        if( ret.trim().equals("")){
-            Log.e("postQ activity", "Empty String");
-            ret = "30";
-        }
-
-        return ret;
-    }
-
-    private void stringToFile( String text, String fileName )
-    {
-        try
-        {
-            File file = new File( fileName );
-
-            // if file doesnt exists, then create it
-            if ( ! file.exists( ) )
-            {
-                file.createNewFile( );
-            }
-
-            FileWriter fw = new FileWriter( file.getAbsoluteFile( ) );
-            BufferedWriter bw = new BufferedWriter( fw );
-            bw.write( text );
-            bw.close( );
-            //System.out.println("Done writing to " + fileName); //For testing
-        }
-        catch( IOException e )
-        {
-            System.out.println("Error: " + e);
-            e.printStackTrace( );
-        }
+//
+//    private String readFromFile(Context context) {
+//
+//        String ret = "";
+//
+//        try {
+//            InputStream inputStream = context.openFileInput("counter.txt");
+//
+//            if ( inputStream != null ) {
+//                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+//                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+//                String receiveString = "";
+//                StringBuilder stringBuilder = new StringBuilder();
+//
+//                while ( (receiveString = bufferedReader.readLine()) != null ) {
+//                    stringBuilder.append(receiveString);
+//                }
+//
+//                inputStream.close();
+//                ret = stringBuilder.toString();
+//            }
+//        }
+//        catch (FileNotFoundException e) {
+//            Log.e("postQ activity", "File not found: " + e.toString());
+//        } catch (IOException e) {
+//            Log.e("postQ activity", "Can not read file: " + e.toString());
+//        }catch (NullPointerException e){
+//            e.printStackTrace();
+//        }
+//
+//        if( ret.trim().equals("")){
+//            Log.e("postQ activity", "Empty String");
+//            ret = "30";
+//        }
+//
+//        return ret;
+//    }
+//
+//    private void stringToFile( String text, String fileName )
+//    {
+//        try
+//        {
+//            File file = new File( fileName );
+//
+//            // if file doesnt exists, then create it
+//            if ( ! file.exists( ) )
+//            {
+//                file.createNewFile( );
+//            }
+//
+//            FileWriter fw = new FileWriter( file.getAbsoluteFile( ) );
+//            BufferedWriter bw = new BufferedWriter( fw );
+//            bw.write( text );
+//            bw.close( );
+//            //System.out.println("Done writing to " + fileName); //For testing
+//        }
+//        catch( IOException e )
+//        {
+//            System.out.println("Error: " + e);
+//            e.printStackTrace( );
+//        }
     } //End method stringToFile
 
 
-}
+
